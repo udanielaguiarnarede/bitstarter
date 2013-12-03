129bd2bab93578fb9bfcc1d1fd39126de99b3f36
Daniel Ernesto Aguiar <udanielaguiarnarede@gmail.com>
2013-12-03 13:59:32 -0100

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var content = fs.readFileSync('index.html', 'utf-8');
+var content = fs.readFileSync('secindex.html', 'utf-8');
 
 app.get('/', function(request, response) {
   response.send(content);
